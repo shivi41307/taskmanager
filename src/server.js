3428046f28b546812fa75f71d6a83dcf56b02565
Sapna Kumari <dffs41307@gmail.com>
2025-12-04 23:10:49 +0530

    Fix Render start script

--- src/server.js
+++ src/server.js
@@ -29,6 +29,6 @@ app.use("/api/tasks", taskRoutes);
 app.use("/api/user", userRoutes);
 
 // server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 export default app;
